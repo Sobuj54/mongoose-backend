082a6373b9f9469e1b30b80a83288b8d2d9fcfda
soubj <sobujahmed124@gmail.com>
2024-02-07 23:56:29 +0600

    ecommerce data model with relations and enum or choices

---
+++ models/ecommerce/product.model.js
@@ -0,0 +1,59 @@
+import mongoose from "mongoose";
+
+const orderItemSchema = new mongoose.Schema({
+  productId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Product",
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+});
+
+const productSchema = new mongoose.Schema(
+  {
+    description: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    productImg: {
+      type: String,
+    },
+    price: {
+      type: Number,
+      default: 0,
+    },
+    stock: {
+      type: Number,
+      default: 0,
+    },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+      required: true,
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    orderItems: {
+      type: [orderItemSchema],
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["PENDING", "CANCELLED", "DELIVERED"], //enum means choices
+    },
+  },
+  { timestamps: true }
+);
+
+export const Product = mongoose.model("Product", productSchema);
