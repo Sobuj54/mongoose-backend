a98c5adf7fbe9cdac0e0fb1cb046e65e2f9701bd
soubj <sobujahmed124@gmail.com>
2024-02-12 23:46:42 +0600

    created hospital data model

--- models/hospital_management/hospital.model.js
+++ models/hospital_management/hospital.model.js
@@ -1,5 +1,29 @@
 import mongoose from "mongoose";
 
-const hospitalSchema = new mongoose.Schema({}, { timestamps: true });
+const hospitalSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    addressLine1: {
+      type: String,
+      required: true,
+    },
+    addressLine2: {
+      type: String,
+    },
+    city: {
+      type: String,
+      required: true,
+    },
+    pinCode: {
+      type: String,
+      required: true,
+    },
+    specializedIn: [{ type: String, required: true }],
+  },
+  { timestamps: true }
+);
 
 export const Hospital = mongoose.model("Hospital", hospitalSchema);
